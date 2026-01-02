eca241f290be774bb2b4a348714ef1cca8b9a2d7
Leonardo Borges <leonardo.borges04@hotmail.com>
2026-01-02 05:07:00 -0300

    feat:  Define configurações da aplicação e realiza primeira migração com Flyway

--- src/main/resources/db/migration/V1__Add_tb_paciente.sql
+++ src/main/resources/db/migration/V1__Add_tb_paciente.sql
@@ -1,5 +1,12 @@
 Create table tb_paciente(
-    id int auto_increament,
-
-
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    cpf  VARCHAR(11) NOT NULL UNIQUE,
+    telefone VARCHAR(20) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    sexo VARCHAR(20) NOT NULL,
+    endereco VARCHAR(255),
+    ativo BOOLEAN DEFAULT TRUE,
+    data_cadastro DATETIME ,
+    data_nascimento DATE NOT NULL
 );
