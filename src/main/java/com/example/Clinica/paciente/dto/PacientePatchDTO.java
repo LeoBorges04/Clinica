9695225e0e0863cb95e9c07690255e02a3622718
Leonardo Borges <leonardo.borges04@hotmail.com>
2026-01-02 03:43:06 -0300

    feat: Adiciona PacientePatchDTO

---
+++ src/main/java/com/example/Clinica/paciente/dto/PacientePatchDTO.java
@@ -0,0 +1,27 @@
+package com.example.Clinica.paciente.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDate;
+
+public class PacientePatchDTO {
+
+    private String nome;
+    @CPF(message = "CPF inválido")
+    private String cpf;
+    @Pattern(
+            regexp = "\\d{10,11}",
+            message = "Telefone deve conter 10 ou 11 dígitos numéricos"
+    )
+    private String telefone;
+    @Email(message = "Email inválido")
+    private String email;
+    private String sexo;
+    private String endereco;
+    @Past(message = "Data de nascimento deve ser no passado")
+    private LocalDate dataNascimento;
+}
