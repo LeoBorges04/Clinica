df79f0c2057f007e408cbb8cf6919ebfeea319cf
Leonardo Borges <leonardo.borges04@hotmail.com>
2026-01-02 07:46:18 -0300

    feat: Adiciona os atributos na entidade e define Enum para UFCRM

--- src/main/java/com/example/Clinica/medico/MedicoEntity.java
+++ src/main/java/com/example/Clinica/medico/MedicoEntity.java
@@ -1,8 +1,23 @@
 package com.example.Clinica.medico;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
 @Table(name="tb_medico")
+@Data
 public class MedicoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String email;
+    private String crm;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "uf_crm", length = 2)
+    private UF ufCrm;
+    private String telefone;
+    private boolean ativo;
+    @Column(unique = true)
+    private String cpf;
 }
