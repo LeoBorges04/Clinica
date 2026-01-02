9efdc626468d13ef5166331af452cb597ed84e71
Leonardo Borges <leonardo.borges04@hotmail.com>
2026-01-02 03:46:33 -0300

    refactor: Adciona campos atualizados da entidade no dto

--- src/main/java/com/example/Clinica/paciente/dto/PacienteResponseDTO.java
+++ src/main/java/com/example/Clinica/paciente/dto/PacienteResponseDTO.java
@@ -1,11 +1,12 @@
 package com.example.Clinica.paciente.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import org.springframework.cglib.core.Local;
 
+import lombok.Data;
+
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Data1
 public class PacienteResponseDTO {
 
     private String nome;
@@ -21,68 +22,7 @@ public class PacienteResponseDTO {
     private String endereco;
     private LocalDateTime dataCadastro;
     private Long id;
+    private LocalDate dataNascimento;
 
-    public LocalDateTime getDataCadastro() {
-        return dataCadastro;
-    }
 
-    public void setDataCadastro(LocalDateTime dataCadastro) {
-        this.dataCadastro = dataCadastro;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSexo() {
-        return sexo;
-    }
-
-    public void setSexo(String sexo) {
-        this.sexo = sexo;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
 }
