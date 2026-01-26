b988927922d826a55aedf06e7ce4e4a00b987c34
LeoBorges04 <leonardo.borges04@hotmail.com>
2026-01-26 18:18:37 -0300

    Primeiro commit

---
+++ src/main/java/com/Clinica/Cliniica/CliniicaApplication.java
@@ -0,0 +1,13 @@
+package com.Clinica.Cliniica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliniicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliniicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Clinica/Cliniica/CliniicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Clinica.Cliniica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliniicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
