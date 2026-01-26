9410ff47f6ae4743e1799d31baeddfbfc324b630
LeoBorges04 <leonardo.borges04@hotmail.com>
2026-01-26 20:17:24 -0300

    feat(paciente): implementa dto para atualização parcial do cadastro paciente

--- src/main/java/com/Clinica/Cliniica/paciente/dto/PacientePatchDto.java
+++ src/main/java/com/Clinica/Cliniica/paciente/dto/PacientePatchDto.java
@@ -1,4 +1,24 @@
 package com.Clinica.Cliniica.paciente.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDate;
+
 public class PacientePatchDto {
+    private String nome;
+    @CPF(message = "CPF inválido")
+    private String cpf;
+    @Pattern(
+            regexp = "\\d{10,11}",
+            message = "Telefone deve conter 10 ou 11 dígitos numéricos"
+    )
+    private String telefone;
+    @Email(message = "Email inválido")
+    private String email;
+    private String endereco;
+    @Past(message = "Data de nascimento deve ser no passado")
+    private LocalDate dataNascimento;
 }
