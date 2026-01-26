f79cbcd97664cfd1ab90a0d507017898f6fa09a2
LeoBorges04 <leonardo.borges04@hotmail.com>
2026-01-26 19:55:31 -0300

    feat(paciente): implementa dto para validação de cadastro do paciente

--- src/main/java/com/Clinica/Cliniica/paciente/dto/PacienteRequestDto.java
+++ src/main/java/com/Clinica/Cliniica/paciente/dto/PacienteRequestDto.java
@@ -1,4 +1,30 @@
 package com.Clinica.Cliniica.paciente.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.*;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.time.LocalDate;
+
 public class PacienteRequestDto {
+    @NotBlank(message = "Nome é obrigatório")
+    private String nome;
+    @NotBlank(message = "CPF é obrigatório")
+    @CPF(message = "CPF inválido")
+    private String cpf;
+    @NotBlank(message = "Endereco é obrigatório")
+    private String endereco;
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email inválido")
+    private String email;
+    @NotBlank(message = "Telefone é obrigatório")
+    @Pattern(
+            regexp = "\\d{10,11}",
+            message = "Telefone deve conter 10 ou 11 digitos numéricos"
+    )
+    private String telefone;
+    @NotNull(message = "Data de nascimento é obrigatória")
+    @Past(message = "A data de nascimento deve ser no passado")
+    @JsonProperty("data_nascimento")
+    private LocalDate data_nascimento;
 }
