1edc98fbd12c0588164f811cf03491a9e75441cb
LeoBorges04 <leonardo.borges04@hotmail.com>
2026-01-26 19:22:30 -0300

    feat(paciente): implementa entidade paciente

--- src/main/java/com/Clinica/Cliniica/paciente/PacienteEntity.java
+++ src/main/java/com/Clinica/Cliniica/paciente/PacienteEntity.java
@@ -1,4 +1,28 @@
 package com.Clinica.Cliniica.paciente;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_paciente")
+@Data
 public class PacienteEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    @Column(unique = true)
+    private String cpf;
+    private String endereco;
+    @Column(unique = true)
+    private String email;
+    private Boolean ativo;
+    private String telefone;
+    private LocalDateTime dataCadastro;
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate data_nascimento;
+
 }
