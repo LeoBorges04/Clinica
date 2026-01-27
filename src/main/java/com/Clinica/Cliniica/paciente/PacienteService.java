1b32a69b55ccc459c62774ce29f2bce571b71183
LeoBorges04 <leonardo.borges04@hotmail.com>
2026-01-27 19:44:20 -0300

    feat(paciente): implementa CRUD do paciente

--- src/main/java/com/Clinica/Cliniica/paciente/PacienteService.java
+++ src/main/java/com/Clinica/Cliniica/paciente/PacienteService.java
@@ -1,4 +1,132 @@
 package com.Clinica.Cliniica.paciente;
 
+import com.Clinica.Cliniica.paciente.dto.PacienteMapper;
+import com.Clinica.Cliniica.paciente.dto.PacientePatchDto;
+import com.Clinica.Cliniica.paciente.dto.PacienteRequestDto;
+import com.Clinica.Cliniica.paciente.dto.PacienteResponseDto;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 public class PacienteService {
+    private final PacienteRepository pacienteRepository;
+    private final PacienteMapper pacienteMapper;
+
+    public PacienteService(PacienteRepository pacienteRepository, PacienteMapper pacienteMapper){
+        this.pacienteRepository = pacienteRepository;
+        this.pacienteMapper = pacienteMapper;
+    }
+    //Cadastra paciente
+    @Transactional
+    public PacienteResponseDto cadastrar(PacienteRequestDto dto){
+        PacienteEntity paciente = pacienteMapper.map(dto);
+        paciente.setData_cadastro(LocalDateTime.now());
+        paciente.setAtivo(true);
+        PacienteEntity pacSalvo = pacienteRepository.save(paciente);
+        return pacienteMapper.map(pacSalvo);
+    }
+
+    //Retorna paciente
+    public PacienteResponseDto listar(Long id){
+         PacienteEntity paciente = pacienteRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Paciente não encontrado"));
+        if(!paciente.getAtivo()){
+            throw new IllegalStateException("Paciente está inativo");
+        }
+        return pacienteMapper.map(paciente);
+    }
+    //Retorna lista de pacientes
+    public List<PacienteResponseDto> listarTodos(){
+        return pacienteRepository.findAllByAtivoTrue().stream().map(PacienteMapper::map).toList();
+    }
+
+    //Atualiza paciente
+    @Transactional
+    public PacienteResponseDto atualizar(Long id, PacienteRequestDto dto){
+        PacienteEntity paciente = pacienteRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Paciente não encontrado"));
+        if(!paciente.getAtivo()){
+            throw new IllegalStateException("Paciente está inativo");
+        }
+
+        if(dto.getCpf() != null){
+            paciente.setCpf(dto.getCpf());
+        }
+
+        if(dto.getData_nascimento() != null){
+            paciente.setData_nascimento(dto.getData_nascimento());
+        }
+
+        if(dto.getEmail() != null){
+            paciente.setEmail(dto.getEmail());
+        }
+        if(dto.getEndereco() != null){
+            paciente.setEndereco(dto.getEndereco());
+        }
+
+        if(dto.getNome() != null){
+            paciente.setNome(dto.getNome());
+        }
+
+        if(dto.getTelefone() != null){
+            paciente.setTelefone(dto.getTelefone());
+        }
+
+        return pacienteMapper.map(paciente);
+
+    }
+    //Atualiza paciente (parcial)
+    @Transactional
+    public PacienteResponseDto atualizarParcial(Long id, PacientePatchDto dto){
+        PacienteEntity paciente = pacienteRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Paciente não encontrado"));
+        if(dto.getCpf() != null){
+            paciente.setCpf(dto.getCpf());
+        }
+
+        if(dto.getDataNascimento() != null){
+            paciente.setData_nascimento(dto.getDataNascimento());
+
+        }
+
+        if(dto.getEmail() != null){
+            paciente.setEmail(dto.getEmail());
+        }
+
+        if(dto.getEndereco() != null){
+            paciente.setEndereco(dto.getEndereco());
+        }
+
+        if(dto.getNome() != null){
+            paciente.setNome(dto.getNome());
+        }
+
+        if(dto.getTelefone() != null){
+            paciente.setTelefone(dto.getTelefone());
+        }
+
+        return pacienteMapper.map(paciente);
+    }
+
+    //Reativa paciente
+    @Transactional
+    public PacienteResponseDto atualiza(Long id){
+        PacienteEntity paciente = pacienteRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Paciente não encontrado"));
+        if(paciente.getAtivo()){
+            throw new IllegalStateException("Paciente já está ativo");
+        }
+        paciente.setAtivo(true);
+        return pacienteMapper.map(paciente);
+
+    }
+    //Desativa paciente (soft delete)
+    @Transactional
+    public void deletar(Long id){
+        PacienteEntity paciente = pacienteRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Paciente não encontrado"));
+        if(!paciente.getAtivo()){
+            throw new IllegalStateException("Paciente já está inativo");
+        }
+        paciente.setAtivo(false);
+    }
 }
