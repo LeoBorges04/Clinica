981ed3c92e7bb416ad8e10b8a9c8edc9fb987a4f
Leonardo Borges <leonardo.borges04@hotmail.com>
2026-01-28 03:36:47 -0300

    chore(medico): estrutura base EspecialidadeController

---
+++ src/main/java/com/Clinica/Cliniica/especialidade/EspecialidadeController.java
@@ -0,0 +1,9 @@
+package com.Clinica.Cliniica.especialidade;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class EspecialidadeController {
+}
