a5fa4c8979671c41c6665db9bc40ee9d23f65128
LeoBorges04 <leonardo.borges04@hotmail.com>
2026-01-28 19:33:59 -0300

    feat(especialidade): implementa classe EspecialidadeResponseDto

---
+++ src/main/java/com/Clinica/Cliniica/especialidade/dto/EspecialidadeResponseDto.java
@@ -0,0 +1,10 @@
+package com.Clinica.Cliniica.especialidade.dto;
+
+import lombok.Data;
+
+@Data
+public class EspecialidadeResponseDto {
+    private String nome;
+    private Long id;
+    private Boolean ativo;
+}
