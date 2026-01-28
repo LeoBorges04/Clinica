d981a785c124f83f156d573ad6d0fd2da07b97be
Leonardo Borges <leonardo.borges04@hotmail.com>
2026-01-28 00:24:24 -0300

    feat(paciente): implementa ResponseEntity como resposta nas rotas

--- src/main/java/com/Clinica/Cliniica/paciente/PacienteController.java
+++ src/main/java/com/Clinica/Cliniica/paciente/PacienteController.java
@@ -4,6 +4,8 @@ import com.Clinica.Cliniica.paciente.dto.PacientePatchDto;
 import com.Clinica.Cliniica.paciente.dto.PacienteRequestDto;
 import com.Clinica.Cliniica.paciente.dto.PacienteResponseDto;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,29 +16,36 @@ public class PacienteController {
     public PacienteController(PacienteService pacienteService){
         this.pacienteService = pacienteService;
     }
-    @PostMapping("/paciente/")
-    public PacienteResponseDto criarPaciente(@Valid @RequestBody PacienteRequestDto dto){
-        return pacienteService.cadastrar(dto);
+    @PostMapping("/paciente")
+    public ResponseEntity<PacienteResponseDto> criarPaciente(@Valid @RequestBody PacienteRequestDto dto){
+        PacienteResponseDto paciente = pacienteService.cadastrar(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(paciente);
+     }
+
+    @GetMapping("/paciente/{id}")
+    public ResponseEntity<PacienteResponseDto> listarPaciente(@PathVariable Long id){
+        PacienteResponseDto paciente =  pacienteService.listar(id);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(paciente);
     }
 
     @GetMapping("/paciente/")
-    public PacienteResponseDto listarPaciente(@PathVariable Long id){
-        return pacienteService.listar(id);
+    public ResponseEntity<List<PacienteResponseDto>> listarTodos(){
+        List<PacienteResponseDto> pacientes = pacienteService.listarTodos();
+        return ResponseEntity.ok(pacientes);
     }
 
-    @GetMapping("/paciente/")
-    public List<PacienteResponseDto> listarTodos(){
-        return pacienteService.listarTodos();
+    @PatchMapping("/paciente/{id}")
+    public ResponseEntity<PacienteResponseDto> atualizar(@PathVariable Long id, @Valid @RequestBody PacientePatchDto paciente){
+        PacienteResponseDto dto = pacienteService.atualizarParcial(id,paciente);
+        return ResponseEntity.ok(dto);
     }
 
-    @PatchMapping("/paciente/")
-    public PacienteResponseDto atualizar(@PathVariable Long id, @RequestBody PacientePatchDto paciente){
-        return pacienteService.atualizarParcial(id, paciente);
-    }
 
-    @DeleteMapping("/paciente/")
-    public void deletar(@PathVariable Long id){
+    @DeleteMapping("/paciente/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id){
         pacienteService.deletar(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
